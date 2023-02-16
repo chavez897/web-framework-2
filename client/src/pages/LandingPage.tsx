b2bcbd506047a94b7affd8e1e517b8e89c4c7c6f
Moanisha <moanisha94@gmail.com>
2023-02-16 14:33:21 -0500

    content from api

--- client/src/pages/LandingPage.tsx
+++ client/src/pages/LandingPage.tsx
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import LandingPageCSS from "../assets/LandingPage.module.css";
 import landingPicture from "../assets/landingPicture.png";
 import SearchBar from "../features/searchTutors/components/SearchBar.tsx";
+import axios from "axios";
+import { useDispatch } from "react-redux";
 
 const LandingPage: React.FC = () => {
+  const [content, setContent] = useState("");
+  const dispatch = useDispatch();
+  useEffect(() => {
+    axios
+      .get("http://localhost:5001/api/v1/contents/63e7a5d14236e852403de3c6")
+      .then((response) => {
+        setContent(response.data.data.description);
+        console.log(response.data.data.description);
+      });
+  }, []);
   return (
     <div className={LandingPageCSS.showcaseArea}>
       <div className={`container ${LandingPageCSS.container}`}>
@@ -11,12 +23,7 @@ const LandingPage: React.FC = () => {
           <div className={LandingPageCSS.title}>
             <h1>Find a tutor,</h1>
             <h1>In any topic that you want.</h1>
-            <p className={LandingPageCSS.text}>
-              Welcome to the community of more than 100 thousand students using
-              wisely their time booking real-time personalized classes with the
-              best tutors around the world. Find classes on any topic for all
-              levels, from kids to seniors.
-            </p>
+            <p className={LandingPageCSS.text}>{content}</p>
             <SearchBar />
           </div>
         </div>
